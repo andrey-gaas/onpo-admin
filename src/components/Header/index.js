6f4e261cc068ca4bd2442edc1fb4c7e3e50f95d6
Гаас Андрей Александрович <Gaas@gpntbsib.ru>
2021-10-20 16:46:06 +0700

    component Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Container } from '../';
-import { Root } from './styles';
+import { Link } from 'react-router-dom';
+import Container from '../Container';
+import { Root, Wrapper, Logo, Title } from './styles';
+import logoSrc from '../../images/logo.svg';
 
 function Header() {
   return (
     <Root>
       <Container>
-        12
+        <Wrapper>
+          <Link to="/">
+            <Logo src={logoSrc} alt="" />
+          </Link>
+          <Title>Личный кабинет</Title>
+        </Wrapper>
       </Container>
     </Root>
   );
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,5 +1,52 @@
 import styled from 'styled-components';
+import {
+  TABLET,
+  LAPTOP,
+  DESCTOP,
+  FS_MD,
+  FS_LGS,
+  FS_LG,
+  BLACK_COLOR,
+} from '../../constants/styles';
 
 export const Root = styled.header`
-  
+  padding: 20px 0px;
+  background-color: #fff;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Logo = styled.img`
+  width: 40px;
+  height: 40px;
+
+  @media ${TABLET} {
+    height: 60px;
+    width: 60px;
+  }
+`;
+
+export const Title = styled.h2`
+  display: none;
+  margin-left: 20px;
+  font-size: ${FS_MD};
+  text-transform: uppercase;
+  color: ${BLACK_COLOR};
+
+  @media ${TABLET} {
+    display: block;
+  }
+
+  @media ${LAPTOP} {
+    margin-left: 40px;
+    font-size: ${FS_LGS};
+  }
+
+  @media ${DESCTOP} {
+    margin-left: 80px;
+    font-size: ${FS_LG};
+  }
 `;
