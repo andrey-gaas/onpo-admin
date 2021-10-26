1e134a071acf5f3fe43e963a0808b771a8037e4a
Гаас Андрей Александрович <Gaas@gpntbsib.ru>
2021-10-27 04:03:26 +0700

    error and loading for reviews page

--- src/containers/Reviews/index.js
+++ src/containers/Reviews/index.js
@@ -3,26 +3,42 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { reviewsFetch } from '../../store/AC/reviews';
-import { Root } from './styles';
+import { Root, Loading, Error } from './styles';
 
-function Reviews({ reviewsFetch }) {
+function Reviews({ reviewsFetch, reviews }) {
+  const { list, error } = reviews;
+
   useEffect(() => {
     reviewsFetch();
   }, [reviewsFetch]);
 
   return (
     <Root>
-      123
+      {
+        list === null &&
+        error === null &&
+        <Loading>Загрузка...</Loading>
+      }
+
+      {
+        error !== null &&
+        <Error>Ошибка загрузки списка отзывов</Error>
+      }
     </Root>
   );
 }
 
 Reviews.propTypes = {
   reviewsFetch: PropTypes.func.isRequired,
+  reviews:      PropTypes.object.isRequired,
 };
+
+const mapStateToProps = ({ reviews }) => ({
+  reviews,
+});
 
 const mapDispatchToProps = dispatch => bindActionCreators({
   reviewsFetch,
 }, dispatch);
 
-export default connect(null, mapDispatchToProps)(Reviews);
+export default connect(mapStateToProps, mapDispatchToProps)(Reviews);
--- src/containers/Reviews/styles.js
+++ src/containers/Reviews/styles.js
@@ -1,3 +1,22 @@
 import styled from 'styled-components';
+import { FS_LGM, BLACK_COLOR, PRIMARY_COLOR } from '../../constants/styles';
 
-export const Root = styled.main``;
+export const Root = styled.main`
+  display: flex;
+  flex-direction: column;
+  height: 100%;;
+`;
+
+export const Loading = styled.div`
+  margin-top: 50px;
+  align-self: center;
+  font-size: ${FS_LGM};
+  color: ${BLACK_COLOR};
+`;
+
+export const Error = styled.div`
+  margin-top: 50px;
+  align-self: center;
+  font-size: ${FS_LGM};
+  color: ${PRIMARY_COLOR};
+`;
